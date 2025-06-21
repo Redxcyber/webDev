f32503f797f2fb36412c79bdd3c00f4be3a0d27d
redxcyber <mrdark9628@gmail.com>
2025-06-21 16:56:25 +0530

    update 35-taskCountDown.js

--- javascript/35-taskCountDown.js
+++ javascript/35-taskCountDown.js
@@ -2,12 +2,28 @@
 
 let seconds = 60;
 
-let countIntervalId = setInterval(() => {
+let timerId = setInterval(() => {
     console.log(`${seconds} sec left !!`);
     seconds--;
 
-    if (seconds < 0) {
+    if (seconds <= 0) {
         console.log("Time's up");
-        clearInterval(countIntervalId);
+        clearInterval(timerId);
     }
 }, 1000);
+
+
+//better approach
+function countdown(seconds) {
+    let timerId = setInterval(() => {
+        console.log(`${seconds} sec left !!`);
+        seconds--;
+        
+        if(seconds <= 0) {
+            console.log("Time's up");
+            clearInterval(timerId)
+        }
+
+    }, 1000);
+}
+countdown(60);
