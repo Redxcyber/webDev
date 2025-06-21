bbd228cf8e3741c9cb5e4f2845a5fd6bdbd08c4b
redxcyber <mrdark9628@gmail.com>
2025-06-21 17:36:31 +0530

    update 35-taskTypingAnimation.js

--- javascript/35-taskTypingAnimation.js
+++ javascript/35-taskTypingAnimation.js
@@ -11,17 +11,19 @@
 //     }
 // }, 1000);
 
-let text = "Pranav singh chandel";
-let i = 0;
+// let text = "Pranav singh chandel";
+// let i = 0;
 
-let typer = setInterval(() => {
-    process.stdout.write(text[i]);
-    i++;
-    if (i >= text.length) {
-        clearInterval(typer);
-        console.log("\nDone!");
-    }
-}, 100);
+// let typer = setInterval(() => {
+//     process.stdout.write(text[i]);
+//     i++;
+//     if (i >= text.length) {
+//         clearInterval(typer);
+//         console.log("\nDone!");
+//     }
+// }, 100);
+
+
 
 /*
     In Node.js, process.stdout.write is a method used to print text to 
@@ -29,4 +31,46 @@ let typer = setInterval(() => {
 
     adapt for browser using document.write or innerHTML.
 */
+
+
+
+
+//--------> Method-01 : setInterval
+
+function typingAnimation(text) {
+    let i = 0;
+
+    let typer = setInterval(() => {
+        process.stdout.write(text[i]);
+        i++;
+        if (i >= text.length) {
+            clearInterval(typer);
+            console.log("\nDone!");
+        }
+    }, 100);
+}
+typingAnimation("Pranav singh chandel");
+
+
+
+
+//---------> method-02 : setTimeout (recursive)
+
+function typingEffect(text) {
+    let index = 0;
+
+    function typeChar() {
+        if (index < text.length) {
+            process.stdout.write(text[index]);
+            index++;
+            setTimeout(typeChar, 100);
+        } else {
+            console.log("\nDone!");
+        }
+    }
+
+    typeChar();
+}
+
+typingEffect("Pranav singh chandel");
 
